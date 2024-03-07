1cdd794da64bd4bf88024ee0f1eafcbfd9e4e5a7
Владіслав Лоєнко <74206645+vloienko@users.noreply.github.com>
2024-03-07 19:59:08 +0200

    added: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Link, Outlet } from 'react-router-dom'
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -17,14 +18,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
+      <nav className="navbar-links">
+        <Link to="/CarMasters/">Home</Link>
+        <Link to="/CarMasters/contact">Contact</Link>
+      </nav>
+     
+      <Outlet />
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -32,4 +33,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home } from './components/Pages/Home.jsx';
+import { Contact } from './components/Pages/Contact.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/CarMasters/",
+    element: <App />,
+    children: [
+      {
+        path: "/CarMasters/",
+        element: <Home />,
+      },
+      {
+        path: "/CarMasters/contact",
+        element: <Contact />,
+      },
+    ]
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
