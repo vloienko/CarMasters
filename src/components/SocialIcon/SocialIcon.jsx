aaa0e7a326405bfee939aff0a4e45f77e6e94e84
Владіслав Лоєнко <74206645+vloienko@users.noreply.github.com>
2024-03-14 17:29:25 +0200

    footer - done, optimized code SocialIcon

--- src/components/SocialIcon/SocialIcon.jsx
+++ src/components/SocialIcon/SocialIcon.jsx
@@ -1,26 +1,26 @@
 import './SocialIcon.scss';
-
 import { Icon } from '../Icon/Icon';
 
 
+// масив для зберігання даних іконок
+const socialIcons = [
+   { id: 'instagram', className: 'svg-instagram' },
+   { id: 'facebook', className: 'svg-facebook' },
+   { id: 'youtube', className: 'svg-youtube' }
+];
+
+
+// використання методу map
 export const SocialIcon = () => {
    return (
       <ul className="social-icon">
-         <li>
-            <a className="social-icon__link" href="#">
-               <Icon id='instagram' className='svg-instagram' />
-            </a>
-         </li>
-         <li>
-            <a className="social-icon__link" href="#">
-               <Icon id='facebook' className='svg-facebook' />
-            </a>
-         </li>
-         <li>
-            <a className="social-icon__link" href="#">
-               <Icon id='youtube' className='svg-youtube' />
-            </a>
-         </li>
+         {socialIcons.map(icon => (
+            <li key={icon.id}>
+               <a className="social-icon__link" href="#">
+                  <Icon id={icon.id} className={icon.className} />
+               </a>
+            </li>
+         ))}
       </ul>
    );
 };
